44ca289e407813df04727a44edebf376be0ccb69
eeexuanlyx <eeexuanlyx@gmail.com>
2024-09-26 17:45:12 +0800

    added solutions till exercise 9

--- app.js
+++ app.js
@@ -120,3 +120,74 @@ const maxOfThree = function (x, y, z) {
 };
 
 console.log("Exercise 6 Result:", maxOfThree(5, 10, 8));
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+let calculateTip = function (x, y) {
+  return Math.round((y / 100) * x);
+};
+
+console.log("Exercise 7 Result:", calculateTip(50, 20));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (x, y) => {
+  if (y === "C") {
+    return ((9 * x) / 5 + 32).toFixed(1) + " Fahrenheit";
+  } else if (y === "F") {
+    return (((x - 32) * 5) / 9).toFixed(1) + " Celsius";
+  }
+};
+
+console.log("Exercise 8 Result:", convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = function (x, y, z) {
+  if (z === "add") {
+    return x + y;
+  } else if (z === "subtract") {
+    return x - y;
+  } else if (z === "multiply") {
+    return x * y;
+  } else if (z === "divide") {
+    return x / y;
+  }
+};
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"));
